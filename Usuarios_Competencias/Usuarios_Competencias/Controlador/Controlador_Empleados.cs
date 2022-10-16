9c1586e7c346ee0745e7c6f1fb5f69f008f8a09c
Luna Hazuki <LunaHazuki2006@outlook.com>
2022-10-16 14:42:11 -0400

    Ya con los controladores hechos

--- Usuarios_Competencias/Usuarios_Competencias/Controlador/Controlador_Empleados.cs
+++ Usuarios_Competencias/Usuarios_Competencias/Controlador/Controlador_Empleados.cs
@@ -15,7 +15,9 @@ namespace Usuarios_Competencias.Controlador
             {
                 using (var db = new EmpledoContext())
                 {
-
+                    empleado.Estado = 'a';
+                    db.Empleados.Add(empleado);
+                    db.SaveChanges();
                 };
             }
             catch (Exception e)
@@ -66,13 +68,69 @@ namespace Usuarios_Competencias.Controlador
             {
                 using (var db = new EmpledoContext())
                 {
-
+                    var empleado = db.Empleados.Find(cedula);
+                    db.Empleados.Remove(empleado);
+                    db.SaveChanges();
                 };
             }
             catch (Exception e)
             {
                 Logger.LogError(e);
-                Mensajes.MensajeError($"Errar inesperado: {e.Message}");
+                Mensajes.MensajeError($"Error inesperado: {e.Message}");
+            }
+        }
+
+        public static List<Empleado>? Listar()
+        {
+            try
+            {
+                using (var db = new EmpledoContext())
+                {
+                    var empleados = db.Empleados.ToList();
+                    return empleados;
+                };
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+                Mensajes.MensajeError($"Error inesperado: {e.Message}");
+                return default;
+            }
+        }
+
+        public static List<Empleado>? Listar_Cedulas(string cedula)
+        {
+            try
+            {
+                using (var db = new EmpledoContext())
+                {
+                    var empleados = db.Empleados.Where(t => t.Cedula.Contains(cedula)).ToList();
+                    return empleados;
+                };
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+                Mensajes.MensajeError($"Error inesperado: {e.Message}");
+                return default;
+            }
+        }
+
+        public static List<Empleado>? Listar_Estados(char estado)
+        {
+            try
+            {
+                using (var db = new EmpledoContext())
+                {
+                    var empleados = db.Empleados.Where(t => t.Estado == estado).ToList();
+                    return empleados;
+                };
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+                Mensajes.MensajeError($"Error inesperado: {e.Message}");
+                return default;
             }
         }
     }
