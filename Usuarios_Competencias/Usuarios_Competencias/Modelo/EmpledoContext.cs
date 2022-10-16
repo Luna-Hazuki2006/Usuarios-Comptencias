f7a0528a4170b4b9f777d034193884aaf145f42f
Luna Hazuki <LunaHazuki2006@outlook.com>
2022-10-16 11:46:27 -0400

    ahora con el dbcontext

---
+++ Usuarios_Competencias/Usuarios_Competencias/Modelo/EmpledoContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Usuarios_Competencias.Modelo
+{
+    public class EmpledoContext : DbContext
+    {
+        public virtual DbSet<Empleado>? Empleados { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.EnableSensitiveDataLogging().IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=UsuariosCompetencia;Trusted_Connection=True;");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Empleado>(t =>
+            {
+                t.HasKey(t => t.Cedula);
+                t.Property(t => t.Estado).IsRequired(true);
+            });
+        }
+    }
+}
