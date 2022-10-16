505fc098a780d29bfff222b3563f960b27242abf
Luna Hazuki <LunaHazuki2006@outlook.com>
2022-10-16 11:21:38 -0400

    algunos problemas con versiones

---
+++ Usuarios/Modelo/Empleado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usuarios.Modelo
+{
+    public class Empleado
+    {
+        public string Cedula { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public DateTime Fecha_Ingreso { get; set; }
+        public char Estado { get; set; }
+    }
+}
