6d2071cd1c8540936d1cf0089e777907de36a1c9
Luna Hazuki <LunaHazuki2006@outlook.com>
2022-10-16 15:19:01 -0400

    ya con un poco de vista

---
+++ Usuarios_Competencias/Usuarios_Competencias/Vista/Lista.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Usuarios_Competencias.Vista
+{
+    partial class Lista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Lista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(550, 209);
+            this.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Lista";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lista";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Usuarios_Competencias/Usuarios_Competencias/Vista/Unico.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Usuarios_Competencias.Vista
+{
+    partial class Unico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanelTodo = new System.Windows.Forms.TableLayoutPanel();
+            this.labelCedula = new System.Windows.Forms.Label();
+            this.labelNombres = new System.Windows.Forms.Label();
+            this.labelApellidos = new System.Windows.Forms.Label();
+            this.labelDireccion = new System.Windows.Forms.Label();
+            this.labelTelefono = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelFecha = new System.Windows.Forms.Label();
+            this.labelEstado = new System.Windows.Forms.Label();
+            this.textBoxCedula = new System.Windows.Forms.TextBox();
+            this.textBoxNombres = new System.Windows.Forms.TextBox();
+            this.textBoxApellidos = new System.Windows.Forms.TextBox();
+            this.textBoxDireccion = new System.Windows.Forms.TextBox();
+            this.textBoxTelefono = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.dateTimePickerFecha = new System.Windows.Forms.DateTimePicker();
+            this.comboBoxEstado = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanelTodo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelTodo
+            // 
+            this.tableLayoutPanelTodo.ColumnCount = 3;
+            this.tableLayoutPanelTodo.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelTodo.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanelTodo.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanelTodo.Controls.Add(this.textBoxTelefono, 1, 4);
+            this.tableLayoutPanelTodo.Controls.Add(this.textBoxDireccion, 1, 3);
+            this.tableLayoutPanelTodo.Controls.Add(this.textBoxApellidos, 1, 2);
+            this.tableLayoutPanelTodo.Controls.Add(this.textBoxNombres, 1, 1);
+            this.tableLayoutPanelTodo.Controls.Add(this.labelCedula, 0, 0);
+            this.tableLayoutPanelTodo.Controls.Add(this.labelNombres, 0, 1);
+            this.tableLayoutPanelTodo.Controls.Add(this.labelApellidos, 0, 2);
+            this.tableLayoutPanelTodo.Controls.Add(this.labelDireccion, 0, 3);
+            this.tableLayoutPanelTodo.Controls.Add(this.labelTelefono, 0, 4);
+            this.tableLayoutPanelTodo.Controls.Add(this.labelEmail, 0, 5);
+            this.tableLayoutPanelTodo.Controls.Add(this.labelEstado, 0, 7);
+            this.tableLayoutPanelTodo.Controls.Add(this.textBoxCedula, 1, 0);
+            this.tableLayoutPanelTodo.Controls.Add(this.textBoxEmail, 1, 5);
+            this.tableLayoutPanelTodo.Controls.Add(this.labelFecha, 0, 6);
+            this.tableLayoutPanelTodo.Controls.Add(this.dateTimePickerFecha, 1, 6);
+            this.tableLayoutPanelTodo.Controls.Add(this.comboBoxEstado, 1, 7);
+            this.tableLayoutPanelTodo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelTodo.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelTodo.Name = "tableLayoutPanelTodo";
+            this.tableLayoutPanelTodo.RowCount = 9;
+            this.tableLayoutPanelTodo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanelTodo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanelTodo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanelTodo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanelTodo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanelTodo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanelTodo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanelTodo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanelTodo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanelTodo.Size = new System.Drawing.Size(744, 339);
+            this.tableLayoutPanelTodo.TabIndex = 0;
+            // 
+            // labelCedula
+            // 
+            this.labelCedula.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelCedula.AutoSize = true;
+            this.labelCedula.Location = new System.Drawing.Point(80, 8);
+            this.labelCedula.Name = "labelCedula";
+            this.labelCedula.Size = new System.Drawing.Size(65, 20);
+            this.labelCedula.TabIndex = 0;
+            this.labelCedula.Text = "Cédula:";
+            // 
+            // labelNombres
+            // 
+            this.labelNombres.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelNombres.AutoSize = true;
+            this.labelNombres.Location = new System.Drawing.Point(66, 45);
+            this.labelNombres.Name = "labelNombres";
+            this.labelNombres.Size = new System.Drawing.Size(79, 20);
+            this.labelNombres.TabIndex = 1;
+            this.labelNombres.Text = "Nombres:";
+            // 
+            // labelApellidos
+            // 
+            this.labelApellidos.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelApellidos.AutoSize = true;
+            this.labelApellidos.Location = new System.Drawing.Point(61, 82);
+            this.labelApellidos.Name = "labelApellidos";
+            this.labelApellidos.Size = new System.Drawing.Size(84, 20);
+            this.labelApellidos.TabIndex = 2;
+            this.labelApellidos.Text = "Apellidos:";
+            // 
+            // labelDireccion
+            // 
+            this.labelDireccion.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelDireccion.AutoSize = true;
+            this.labelDireccion.Location = new System.Drawing.Point(64, 119);
+            this.labelDireccion.Name = "labelDireccion";
+            this.labelDireccion.Size = new System.Drawing.Size(81, 20);
+            this.labelDireccion.TabIndex = 3;
+            this.labelDireccion.Text = "Dirección:";
+            // 
+            // labelTelefono
+            // 
+            this.labelTelefono.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelTelefono.AutoSize = true;
+            this.labelTelefono.Location = new System.Drawing.Point(69, 156);
+            this.labelTelefono.Name = "labelTelefono";
+            this.labelTelefono.Size = new System.Drawing.Size(76, 20);
+            this.labelTelefono.TabIndex = 4;
+            this.labelTelefono.Text = "Teléfono:";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(90, 193);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(55, 20);
+            this.labelEmail.TabIndex = 5;
+            this.labelEmail.Text = "Email:";
+            // 
+            // labelFecha
+            // 
+            this.labelFecha.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelFecha.AutoSize = true;
+            this.labelFecha.Location = new System.Drawing.Point(12, 230);
+            this.labelFecha.Name = "labelFecha";
+            this.labelFecha.Size = new System.Drawing.Size(133, 20);
+            this.labelFecha.TabIndex = 6;
+            this.labelFecha.Text = "Fecha de ingreso:";
+            // 
+            // labelEstado
+            // 
+            this.labelEstado.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelEstado.AutoSize = true;
+            this.labelEstado.Location = new System.Drawing.Point(83, 267);
+            this.labelEstado.Name = "labelEstado";
+            this.labelEstado.Size = new System.Drawing.Size(62, 20);
+            this.labelEstado.TabIndex = 7;
+            this.labelEstado.Text = "Estado:";
+            // 
+            // textBoxCedula
+            // 
+            this.textBoxCedula.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxCedula.BackColor = System.Drawing.Color.SeaShell;
+            this.tableLayoutPanelTodo.SetColumnSpan(this.textBoxCedula, 2);
+            this.textBoxCedula.Location = new System.Drawing.Point(151, 5);
+            this.textBoxCedula.Name = "textBoxCedula";
+            this.textBoxCedula.Size = new System.Drawing.Size(590, 27);
+            this.textBoxCedula.TabIndex = 8;
+            // 
+            // textBoxNombres
+            // 
+            this.textBoxNombres.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxNombres.BackColor = System.Drawing.Color.SeaShell;
+            this.tableLayoutPanelTodo.SetColumnSpan(this.textBoxNombres, 2);
+            this.textBoxNombres.Location = new System.Drawing.Point(151, 42);
+            this.textBoxNombres.Name = "textBoxNombres";
+            this.textBoxNombres.Size = new System.Drawing.Size(590, 27);
+            this.textBoxNombres.TabIndex = 9;
+            // 
+            // textBoxApellidos
+            // 
+            this.textBoxApellidos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxApellidos.BackColor = System.Drawing.Color.SeaShell;
+            this.tableLayoutPanelTodo.SetColumnSpan(this.textBoxApellidos, 2);
+            this.textBoxApellidos.Location = new System.Drawing.Point(151, 79);
+            this.textBoxApellidos.Name = "textBoxApellidos";
+            this.textBoxApellidos.Size = new System.Drawing.Size(590, 27);
+            this.textBoxApellidos.TabIndex = 10;
+            // 
+            // textBoxDireccion
+            // 
+            this.textBoxDireccion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDireccion.BackColor = System.Drawing.Color.SeaShell;
+            this.tableLayoutPanelTodo.SetColumnSpan(this.textBoxDireccion, 2);
+            this.textBoxDireccion.Location = new System.Drawing.Point(151, 116);
+            this.textBoxDireccion.Name = "textBoxDireccion";
+            this.textBoxDireccion.Size = new System.Drawing.Size(590, 27);
+            this.textBoxDireccion.TabIndex = 11;
+            // 
+            // textBoxTelefono
+            // 
+            this.textBoxTelefono.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTelefono.BackColor = System.Drawing.Color.SeaShell;
+            this.tableLayoutPanelTodo.SetColumnSpan(this.textBoxTelefono, 2);
+            this.textBoxTelefono.Location = new System.Drawing.Point(151, 153);
+            this.textBoxTelefono.Name = "textBoxTelefono";
+            this.textBoxTelefono.Size = new System.Drawing.Size(590, 27);
+            this.textBoxTelefono.TabIndex = 12;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxEmail.BackColor = System.Drawing.Color.SeaShell;
+            this.tableLayoutPanelTodo.SetColumnSpan(this.textBoxEmail, 2);
+            this.textBoxEmail.Location = new System.Drawing.Point(151, 190);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(590, 27);
+            this.textBoxEmail.TabIndex = 13;
+            // 
+            // dateTimePickerFecha
+            // 
+            this.dateTimePickerFecha.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanelTodo.SetColumnSpan(this.dateTimePickerFecha, 2);
+            this.dateTimePickerFecha.Location = new System.Drawing.Point(151, 229);
+            this.dateTimePickerFecha.Name = "dateTimePickerFecha";
+            this.dateTimePickerFecha.Size = new System.Drawing.Size(590, 27);
+            this.dateTimePickerFecha.TabIndex = 14;
+            // 
+            // comboBoxEstado
+            // 
+            this.comboBoxEstado.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxEstado.BackColor = System.Drawing.Color.SeaShell;
+            this.tableLayoutPanelTodo.SetColumnSpan(this.comboBoxEstado, 2);
+            this.comboBoxEstado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxEstado.FormattingEnabled = true;
+            this.comboBoxEstado.Items.AddRange(new object[] {
+            "Activo",
+            "De vacaciones",
+            "De permiso",
+            "Jubilado"});
+            this.comboBoxEstado.Location = new System.Drawing.Point(151, 263);
+            this.comboBoxEstado.Name = "comboBoxEstado";
+            this.comboBoxEstado.Size = new System.Drawing.Size(590, 28);
+            this.comboBoxEstado.TabIndex = 15;
+            // 
+            // Unico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(744, 339);
+            this.Controls.Add(this.tableLayoutPanelTodo);
+            this.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Unico";
+            this.Text = "Unico";
+            this.tableLayoutPanelTodo.ResumeLayout(false);
+            this.tableLayoutPanelTodo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanelTodo;
+        private Label labelCedula;
+        private Label labelNombres;
+        private Label labelApellidos;
+        private Label labelDireccion;
+        private Label labelTelefono;
+        private Label labelEmail;
+        private Label labelFecha;
+        private Label labelEstado;
+        private TextBox textBoxCedula;
+        private TextBox textBoxTelefono;
+        private TextBox textBoxDireccion;
+        private TextBox textBoxApellidos;
+        private TextBox textBoxNombres;
+        private TextBox textBoxEmail;
+        private DateTimePicker dateTimePickerFecha;
+        private ComboBox comboBoxEstado;
+    }
+}
